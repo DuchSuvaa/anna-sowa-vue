54f719973f957220048ee5fdb7d57623d504f0e6
DuchSuvaa <bga.sowa@gmail.com>
2026-04-26 23:16:49 +0200

    works section basic styling

---
+++ src/components/WorkItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <li class="work-item">
+    <img :src="work.image" alt="">
+    <h3>{{ work.title }}</h3>
+    <span>{{ work.year }}</span>
+    <span>{{ work.info }}</span>
+    <span>{{ work.description }}</span>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps([ 'work' ])
+
+</script>
+
+<style lang="scss" scoped>
+li.work-item {
+  width: 49%;
+  border-left: 2px solid $black;
+  font-size: 2.4rem;
+  padding-left: 3rem;
+  margin-bottom: 4rem;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  h3 {
+    margin: 0;
+    font-size: 2rem;
+  }
+  span {
+    display: block;
+    font-size: 2rem;
+  }
+}
+
+</style>
--- src/views/WorksPage.vue
+++ src/views/WorksPage.vue
@@ -2,11 +2,18 @@
   <section id="works">
     <div class="content">
       <div v-if="works && works.length">
-        <div v-for="work in works" :key="work.id">
-          <img :src="work.image" alt="">
-          <h2>{{ work.title }}</h2>
-          <h2>{{ work.year }}</h2>
-        </div>
+        <h2>Chamber Music</h2>
+        <ul>
+          <WorkItem :work="work" v-for="work in chamberWorks" :key="work.id"/>
+        </ul>
+        <h2>Installation</h2>
+        <ul>
+          <WorkItem :work="work" v-for="work in installationWorks" :key="work.id"/>
+        </ul>
+        <h2>Orchestral Music</h2>
+        <ul>
+          <WorkItem :work="work" v-for="work in orchestralWorks" :key="work.id"/>
+        </ul>
       </div>
       <p v-else>No works found</p>
       <button v-if="hasMore && works.length" @click="loadMore" class="load-more-btn">{{ $t('general.load-more') }} </button>
@@ -15,32 +22,41 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useStore } from '../pinia/store'
+import WorkItem from '../components/WorkItem.vue'
 
 const store = useStore()
 const works = ref([])
-const lastDoc = ref(null)
-const hasMore = ref(true)
 
-const loadMore = async () => {
-  const result = await store.getPaginatedCollection('works', lastDoc.value, 10, null)
-  if (result.docs.length > 0) {
-    works.value.push(...result.docs)
-    lastDoc.value = result.lastVisible
-  }
-  if (result.docs.length < 10) {
-    hasMore.value = false
-  }
-}
+const chamberWorks = computed(() => works.value.filter(w => w['music-type'] === 'Chamber' || w['music-type'] === 'chamber'))
+const installationWorks = computed(() => works.value.filter(w => w['music-type'] === 'Installation' || w['music-type'] === 'Installations'))
+const orchestralWorks = computed(() => works.value.filter(w => w['music-type'] === 'Orchestral' || w['music-type'] === 'orchestral'))
 
 onMounted(async () => {
-  await loadMore()
+  works.value = await store.getCollection('works')
 })
 </script>
 
 <style lang="scss" scoped>
 #works {
   background-image: url('../public/bg-works.jpg');
+  .content {
+    div {
+      h2 {
+        text-align: center;
+        text-transform: uppercase;
+        margin-bottom: 4rem;
+        font-size: 3rem;
+      }
+      ul {
+        width: 100%;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        flex-flow: row wrap;
+      }
+    }
+  }
 }
 </style>
