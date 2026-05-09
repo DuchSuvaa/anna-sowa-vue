b31875a28474becf634e52b97e883219ec3e10bf
DuchSuvaa <bga.sowa@gmail.com>
2026-05-09 16:53:57 +0200

    news section styling

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -7,11 +7,21 @@
         </button>
       </div>
       <ul class="news-list" v-if="news && news.length">
-        <li v-for="item in news" :key="item.id">
-          <h2>{{ item.time?.[locale] }}</h2>
-          <p>{{ item.venue?.[locale] }}</p>
-          <p>{{ item.description?.[locale] }}</p>
-          <p>{{ item.performed?.[locale] }}</p>
+        <li v-for="item in news" :key="item.id" class="newsItem">
+          <div class="datePlace">
+            <div class="datePlaceItem" v-if="item.time?.[locale]">
+              <img src="/date.png" alt="Date" />
+              {{ item.time?.[locale] }}
+            </div>
+            <div class="datePlaceItem" v-if="item.venue?.[locale]">
+              <img src="/place.png" alt="Place" />
+              {{ item.venue?.[locale] }}
+            </div>
+          </div>
+          <div class="newsText">
+            <div v-if="item.description?.[locale]">{{ item.description?.[locale] }}</div>
+            <div v-if="item.performed?.[locale]">{{ item.performed?.[locale] }}</div>
+          </div>
         </li>
       </ul>
       <p v-else>No news found</p>
@@ -80,6 +90,90 @@ onMounted(async () => {
     
     &:hover {
       background-color: rgba(255, 255, 255, 1);
+    }
+  }
+}
+
+.news-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.newsItem {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  border-bottom: 1px solid #343333;
+  margin-top: 4rem;
+  padding-bottom: 4rem;
+  font-size: 2.4rem;
+  line-height: 3rem;
+  color: #222;
+
+  &:first-child {
+    margin-top: 0;
+  }
+  
+  &:last-child {
+    border-bottom: none;
+  }
+
+  .datePlace {
+    width: 49%;
+    .datePlaceItem {
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      justify-content: flex-start;
+      margin-bottom: 1.2rem;
+      
+      &:last-child {
+        margin-bottom: 0;
+      }
+      
+      img {
+        margin-right: 2.4rem;
+        height: 2.4rem;
+        width: auto;
+      }
+    }
+  }
+
+  .newsText {
+    width: 49%;
+    div {
+      margin-bottom: 1rem;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+}
+
+@media (max-width: 1100px) {
+  .newsItem {
+    flex-flow: column nowrap;
+    padding-bottom: 2rem;
+    
+    .datePlace, .newsText {
+      width: 100%;
+    }
+    
+    .datePlace {
+      margin-bottom: 2rem;
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  .newsItem {
+    font-size: 1.8rem;
+    line-height: 2.4rem;
+
+    .datePlace .datePlaceItem img {
+      margin-right: 1.5rem;
+      height: 2rem;
     }
   }
 }
