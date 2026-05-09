e797acf337c2533bc8cd408ab49ddcbc1c23534b
DuchSuvaa <bga.sowa@gmail.com>
2026-05-09 16:50:54 +0200

    compostions section styling

--- src/views/CompositionsPage.vue
+++ src/views/CompositionsPage.vue
@@ -7,10 +7,23 @@
         </button>
       </div>
       <ul class="compositions-list" v-if="compositions && compositions.length">
-        <li v-for="composition in compositions" :key="composition.id">
-          <h2>{{ composition.name?.[locale] }}</h2>
-          <p>{{ composition.year }}</p>
-          <p>{{ composition.instrumentation?.[locale] }}</p>
+        <li v-for="composition in compositions" :key="composition.id" class="compItem">
+          <h3 class="compTitle">{{ composition.name?.[locale] }}</h3>
+          
+          <div class="dateInstru">
+            <div class="dateInstruItem" v-if="composition.year">
+              <img src="/date.png" alt="Date" />
+              {{ composition.year }}
+            </div>
+            <div class="dateInstruItem" v-if="composition.instrumentation?.[locale]">
+              <img src="/instrument.png" alt="Instrument" />
+              {{ composition.instrumentation?.[locale] }}
+            </div>
+          </div>
+          
+          <div class="compText" v-if="composition.performed?.[locale]">
+            <div>{{ composition.performed?.[locale] }}</div>
+          </div>
         </li>
       </ul>
       <p v-else>No compositions found</p>
@@ -79,6 +92,86 @@ onMounted(async () => {
     
     &:hover {
       background-color: rgba(255, 255, 255, 1);
+    }
+  }
+}
+
+.compositions-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.compItem {
+  display: flex;
+  flex-flow: column nowrap;
+  font-size: 2.4rem;
+  margin: 4rem auto;
+  padding-bottom: 4rem;
+  border-bottom: 1px solid black;
+  color: #222;
+
+  .compTitle {
+    width: 100%;
+    font-size: 2.8rem !important;
+    font-weight: normal;
+    margin: 0;
+  }
+
+  .dateInstru {
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: flex-start;
+
+    .dateInstruItem {
+      width: auto;
+      display: flex;
+      flex-flow: row nowrap;
+      margin-top: 4rem;
+      margin-bottom: 4rem;
+      margin-right: 9rem;
+      align-items: flex-start;
+
+      img {
+        display: block;
+        margin-right: 3rem;
+        width: auto;
+        height: auto;
+      }
+    }
+  }
+
+  .compText {
+    width: 100%;
+  }
+}
+
+@media (max-width: 1100px) {
+  .compItem {
+    .dateInstru {
+      flex-flow: column nowrap;
+
+      .dateInstruItem {
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+        margin-right: 0;
+      }
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  .compItem {
+    font-size: 1.8rem;
+    .compTitle {
+      font-size: 2.2rem !important;
+    }
+    
+    .dateInstru .dateInstruItem img {
+      margin-right: 1.5rem;
+      max-width: 3rem;
     }
   }
 }
